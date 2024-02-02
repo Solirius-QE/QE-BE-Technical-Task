97591ff62c1c066c6352976621c5a0706dac945d
Georgina <georgina.hall1@hmcts.net>
2024-02-02 14:17:04 +0000

    initial commit

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -8,19 +8,10 @@ import {
   searchItems,
 } from "../controllers/productControllers.js";
 
-//1. Retrieve all Products
 route.get("/allProducts", getAllProductController);
-
-//2. When creating a product, this API handle creating/updating/deleting variants also.
 route.post("/createProduct", createProductController);
-
-// 3. Delete the Product by its id routes
 route.delete("/product/:productId", deleteProductByIdController);
-
-//5. handling Update Product route with the help of productId
 route.patch("/updateProduct/:productId", updateProductById);
-
-// 6. Search API endpoint
 route.get("/product/search", searchItems);
 
 export default route;
